54ebf1e4f47d9a5c4e3f4a8af0a91044cd68cf5f
LaMovie <156336185+LaMovie@users.noreply.github.com>
2025-05-07 17:54:56 -0400

    Add files via upload

--- PELIS.js
+++ PELIS.js
@@ -534,7 +534,8 @@ var PELIS = [
   { name: "🍿La Mujer En Las Sombras", url: "https://drive.google.com/file/d/1U_91j6dWT23QHnhca5wm8tP_ZnYATw0T/view?usp=drivesdk" }, 
   { name: "🍿911", url: "https://drive.google.com/file/d/1lqTiLDezenKB_6ZKfYO1MX22hLkBA-sH/view?usp=drivesdk" }, 
   { name: "🍿Tierra Plana", url: "https://drive.google.com/file/d/18-Gw04kF5JE6vBN0b2EKiQDKUstOQp8r/view?usp=drivesdk" }, 
-  { name: "📺Sport Tv", url: "https://lamovie.github.io/Sport-Tv/" }   
+  { name: "📺Sport Tv", url: "https://lamovie.github.io/Sport-Tv/" }, 
+  { name: "🍿El Juego Del Calamar 2", url: "https://drive.google.com/drive/folders/1QHu4Br2gxdwj66KrlGPO-oE5vuuJugEp" }   
 ];
 
 
